dc1f4d04eb3a792ff5742f2cdae1144d04722a35
John Lorenz IV <john.darc.4@gmail.com>
2020-10-30 03:07:16 -0700

    Loadable profile functionality added

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # John Lorenz IV 
 import dungeon as d
-import player
+import player as p
 DEFAULT_DIFFICULTY = 1
+
+TEST_PROFILE = {
+    'name' : 'Cuatro',
+    'health': 150,
+    'attack': 25,
+    'defense': 10,
+    'intellect': 10,
+    'role': 'admin',
+    'charisma': 100,
+    'luck': 100,
+    'perception': 100,
+}
 
 def play():
     dungeon = d.dungeon(DEFAULT_DIFFICULTY)
-    dungeon.test()
+    admin = p.player(TEST_PROFILE)
+    admin.printStats()
+    
 
 def main():
     play()
--- player.py
+++ player.py
@@ -7,11 +7,60 @@ class player:
             self.health = 100
             self.attack = 10
             self.defense = 10
+            self.intellect = 10
+            self.role = combatClassType()
 
-    def load(self):
+    def load(self, profile):
+        # if a player profile exists, unpack it and pass as a dictionary
         self.name = profile['name']
+        self.attack = profile['attack']
         self.health = profile['health']
         self.defense = profile['defense']
+        self.intellect = profile['intellect']
+        self.role = combatClassType(profile=profile)
+
+    def printStats(self):
+        print('Player Stats:')
+        print('health:', self.health)
+        print('attack:', self.attack)
+        print('defense:', self.defense)
+        print('intellect:', self.intellect)
+        self.role.printStats()
+
+class combatClassType:
+    #TODO: This method isn't very self-descriptive. Explore using @classmethods in the future as a replacement
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            self.load(kwargs['profile'])
+        else:
+            self.role = self.selectCombatRole()
+            self.charisma = 0
+            self.luck = 0
+            self.perception = 0
+
+    def load(self, profile):
+        self.role = profile['role']
+        self.charisma = profile['charisma']
+        self.luck = profile['luck']
+        self.perception = profile['perception']
+
+    def selectCombatRole(self):
+        pass
+
+    def menu(self):
+        print('Please select a class for your adventurer.')
+        print('1. Barbarian')
+        print('2. Thief')
+        print('3. Knight')
+        print('4. Bard')
+        print('5. Mercenary')
+
+    def printStats(self):
+        print('Combat Role stats:')
+        print('role:', self.role)
+        print('charisma:', self.charisma)
+        print('luck:', self.luck)
+        print('perception:', self.perception)
 
 if __name__ == "__main__":
     pass
